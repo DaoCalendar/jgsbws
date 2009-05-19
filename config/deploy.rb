f466b1f4333db19be982be2f74544e62400261ed
Terry Smith <tersmi1@gmail.com>
2009-05-19 16:45:50 -0400

    Changed to data from bbg website

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,75 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+# Your Applications "Name"
+set :application, "Application Name"
 
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
+# The URL to your applications repository
+set :repository,  "SVN URL"
 
-# If you aren't using Subversion to manage your source code, specify
-# your SCM below:
-# set :scm, :subversion
+# Uncomment this line if you're using SVN.  It makes deployments much faster
+set :deploy_via, :remote_cache
 
-role :app, "your app-server here"
-role :web, "your web-server here"
-role :db,  "your db-server here", :primary => true
+# Require subversion to do an export instead of a checkout.
+set :checkout, 'export'
+
+# The user you are using to deploy with (This user should have SSH access to your server)
+set :user, "Your SSH User"
+
+# We want to deploy everything under your user, and we don't want to use sudo
+set :use_sudo, false
+
+# Where to deploy your application to.
+set :deploy_to, "/domains/Your Domain/"
+
+# -------------------------------- Server Definitions --------------------------------
+# Define the hostname of your server.  If you have multiple servers for multiple purposes, we can define those below as well.
+set :server_name, "sh01.blueboxgrid.com"
+
+# We're assuming you're using a single server for your site, but if you have a seperate asset server or database server, you can specify that here.
+role :app, server_name
+role :web, server_name
+role :db,  server_name, :primary => true
+
+
+# -------------------------------- Final Config --------------------------------
+# This configuration option is helpful when using svn+ssh but doesn't hurt anything to leave it enabled always.
+default_run_options[:pty] = true 
+
+namespace :deploy do
+  task :restart do
+    run "touch #{deploy_to}current/tmp/restart.txt"
+  end
+  
+  task :start do
+    run "cd #{deploy_to} && ln -s current/public public_html"
+    run "touch #{deploy_to}current/tmp/restart.txt"
+  end
+  
+  task :symlink do
+    run "cd #{deploy_to} && rm current ; ln -s releases/#{release_name} current"
+    run <<-CMD
+      rm -rf #{latest_release}/log /home/#{user}/#{latest_release}/public/system #{latest_release}/tmp/pids &&
+      ln -s /home/#{user}#{shared_path}/log /home/#{user}#{latest_release}/log &&
+      ln -s /home/#{user}#{shared_path}/system /home/#{user}#{latest_release}/public/system &&
+      ln -s /home/#{user}#{shared_path}/pids /home/#{user}#{latest_release}/tmp/pids
+    CMD
+  end
+  
+  task :stop do
+    run "rm #{deploy_to}public_html"
+  end
+
+  namespace :web do
+    task :disable do
+      run "cd #{deploy_to} && rm public_html && ln -s static_site public_html"
+    end
+
+    task :enable do
+      run "cd #{deploy_to} && rm public_html && ln -s current/public public_html"
+    end
+  end
+
+end
+
+task :after_setup do
+  run "rm -rf #{deploy_to}public_html"
+  run "mkdir #{deploy_to}static_site"
+end
