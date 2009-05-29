422fc2ba52529751ac2145109f227c16b8748dd9
Terry Smith <tersmi1@gmail.com>
2009-05-29 10:14:09 -0400

    Had to change this one...

--- db/migrate/020_add_nba.rb
+++ db/migrate/020_add_nba.rb
@@ -3,7 +3,7 @@ require "migration_helpers"
 extend MigrationHelpers
 # NBA season is october to june
 def self.up
-	nba2008a = IO.readlines('../../joe guy nba 2008/jgsbws nba2008.dat')
+	nba2008a = IO.readlines(File.dirname(__FILE__) + '/../../public/jgsbws nba2008.dat')
 #		puts nba2008a.length
 #		sleep 3
 		nba2008a.reject!{|g|g.include?('*')}
