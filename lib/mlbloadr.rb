03b700771f892b6d73c54bc24ecfd9d9e73db328
Terry Smith <tersmi1@gmail.com>
2009-07-28 13:40:01 -0400

    basically got the update fixed finally

--- lib/mlbloadr.rb
+++ lib/mlbloadr.rb
@@ -1,4 +1,4 @@
-def 	makeprecord(p, gstruct)
+def 	makeprecord(p, gstruct, prevdate, teamleague)
 	p.game_date_time	=  gstruct.date
 	seasonnumber		+= 1 if ! prevdate.nil? and ((p.game_date_time - prevdate) / Secondsperday)  > 60 # time diff in days
 	prevdate		=  p.game_date_time.dup
@@ -59,22 +59,39 @@ def	mlbloader(dataarray)
 		puts "dataarray.length #{dataarray.length} g.inspect #{g.inspect} gstruct #{gstruct.inspect}"
 		p		=	nil
 		pmake		=	nil
-		addedp		=	false
+#		addedp		=	false
 #		t		=	d[0].split("/")
 #		pa		=	Prediction.find_all_by_game_date_time(Time.local(2000+t[2].to_i, t[1], t[0]))
-		pa		=	Prediction.find_all_by_game_date_time(gstruct.date)
+#		puts "looking for date #{gstruct.date.inspect}"
+#		pa		=	Prediction.find_all_by_game_date_time(gstruct.date)
+		sqlstr  = "SELECT * from predictions WHERE predictions.home_team_id = #{gstruct.home} AND predictions.away_team_id = #{gstruct.away}"
+		puts "seeking on this sql str #{sqlstr}"
+		pa   = Prediction.find_by_sql(sqlstr)
+		puts "pa length before date filter is #{pa.length}"
+		pa.delete_if{|g|
+			g["game_date_time"].year     != gstruct.date.year or 
+			g["game_date_time"].month    != gstruct.date.month or 
+			g["game_date_time"].day      != gstruct.date.day
+	        }
+		puts "pa length after date filter is #{pa.length}"
 #		raise p.inspect
 #		if p.length
-		pa.delete_if{|dfg|!(dfg["home_team_id"]		==	gstruct.home)}
-		pa.delete_if{|dfg|!(dfg["away_team_id"]		==	gstruct.away)}
+#		puts "pa length before home team filter is #{pa.length}"
+#		pa.delete_if{|dfg|!(dfg["home_team_id"]		==	gstruct.home)}
+#		puts "pa length after home team filter is #{pa.length}"
+#		pa.delete_if{|dfg|!(dfg["away_team_id"]		==	gstruct.away)}
+#		puts "pa length after away team filter is #{pa.length}"
 		raise "pa.length #{pa.length} pa.inspect #{pa.inspect}" if pa.length	>	1
-		addedp		=	true		if	pa.length	==	0
-		puts "added prediction" if addedp
-		puts "prediction already there" unless addedp
+#		addedp		=	true		if	pa.length	==	0
+#		puts "added prediction" if addedp
+#		puts "prediction already there" unless addedp
 		# sleep 2
 		pmake		=	pa.length
-		p		=	Prediction.new	if	pa.length	==	0
-		p		=	pa.first	if	pa.length	==	1
+		newprec		=	nil
+		newprec		=	(pa.length == 0)
+		raise "newprec is nil" if newprec.nil?
+		p		=	Prediction.new	if	newprec
+		p		=	pa.first	unless	newprec
 #		raise "pa.length #{pa.length} p.inspect #{p.inspect}" if p.id.nil?
 		begin
 			p.week	=	gstruct.day
@@ -82,45 +99,44 @@ def	mlbloader(dataarray)
 #			print e, "\n"
 			raise "e #{e.inspect} d[1].to_i #{d[1].to_i} d[1] #{d[1].inspect} d.inspect #{d.inspect} p.inspect #{p.inspect}"
 		end
-		p.season		=	seasonnumber
-		p			=	makeprecord(p, gstruct)
-		pid			=	p.id
+		p.season	=	seasonnumber
+		puts "newprec is #{newprec.inspect}"
+		if newprec
+			# create new record
+			p	=	makeprecord(p, gstruct, prevdate, teamleague)
+		else
+			# update current one
+			unless (p.actual_home_score == gstruct.homescore && p.actual_away_score == gstruct.awayscore)
+				p.update_attribute(:actual_home_score, gstruct.homescore)
+				p.update_attribute(:actual_away_score, gstruct.awayscore)
+			end
+		end
+		pid		=	p.id
 		# sleep 3
 		# now create or update the baseball bet table
-=begin
-  create_table "baseball_bets", :force => true do |t|
-    t.integer  "pred_id"
-    t.float    "rlhome"
-    t.integer  "rlhodds"
-    t.float    "rlhprob"
-    t.float    "rlaway"
-    t.integer  "rlaodds"
-    t.float    "rlaprob"
-    t.float    "ou"
-    t.integer  "overodds"
-    t.float    "overprob"
-    t.integer  "underodds"
-    t.float    "underprob"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-	:date, :day, :home, :hlambda, :homescore, :away, :alambda, 
-	:awayscore, :homemoneyline, :awaymoneyline, :probhomewin, :overunder, :oline, :uline, 
-	:probtotalover, :homerunlinespread, :homerunline, :probhrlcover, :awayrunlinespread, 
-	:awayrunline, :probarlcover)
-=end
 		bbb		= BaseballBet.find_by_pred_id(pid)
 #		raise "bbb.type #{bbb.type} bbb.nil? #{bbb.nil?}"
+		newbbrec	=	nil
+		puts "newbbrec is #{newbbrec.inspect}"
 		if bbb.nil?  # not found
 			nbp	= BaseballBet.new
 			puts "made new bb bet"
+			newbbrec= true
 		else
 			nbp	= bbb.dup
 			puts "bb bet already there #{bbb.inspect}"
 			puts "bb bet created because predid is #{nbp.pred_id}"  unless nbp.pred_id == pid
 			nbp	= BaseballBet.new unless nbp.pred_id == pid
+			newbbrec= false
 		end
+		raise "newbbrec is nil" if newbbrec.nil?
 		nbp.pred_id	= pid # copy over 
-		makebbrecord(nbp, gstruct)
+		if newbbrec
+			# create new record
+			makebbrecord(nbp, gstruct)
+		else
+			# update current one
+#			nbp.update_attributes
+		end
 	}
 end # nba loader
