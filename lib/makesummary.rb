c35619894588c9122b7e2150e912cc4e6f8e7e01
Terry Smith <tersmi1@gmail.com>
2009-08-05 01:28:49 -0400

    changed to span from divs

--- lib/makesummary.rb
+++ lib/makesummary.rb
@@ -19,9 +19,9 @@ def	ms(wsh, ysh, week, header=nil, gaptitle	=	'Week', gc	=	0)
 		ysudiv		=	Rdiv	if	ysuw	<	ysul
 		ysudiv		=	Ydiv	if	ysuw	==	ysul
 		wpcw		=	(100.0 * wsuw / wsut).r2
-		wpcw		=	0.0	if	wpcw.nan?
+#		wpcw		=	0.0	if	wpcw.nan?
 		ypcw		=	(100.0 * ysuw / ysut).r2
-		ypcw		=	0.0	if	ypcw.nan?
+#		ypcw		=	0.0	if	ypcw.nan?
 
 		watsw	=	wsh['atsright']
 		watst	=	wsh['ats']
@@ -96,28 +96,28 @@ def	ms(wsh, ysh, week, header=nil, gaptitle	=	'Week', gc	=	0)
 		
 
 		# week
-		ta	<<	"<td>#{gaptitle}#{wsudiv} #{wsuw} SU wins #{wsul} loses #{wpcw} % win</div></td>"
+		ta	<<	"<td>#{gaptitle}#{wsudiv} #{wsuw} SU wins #{wsul} loses #{wpcw} % win#{Ediv}</td>"
 		if (wplhw + wplaw + wplhl + wplal)	>	0
-			ta	<<	"<td>#{gaptitle}#{wplhdiv} PL Home #{wplhw} wins #{wplhl} loses #{(100.0 * wplhw / (wplht == 0 ? 1 : wplht)).r2} % win #{wplhp.r2.commify} profit</div></td>"
-			ta	<<	"<td>#{gaptitle}#{wpladiv} PL Away #{wplaw} wins #{wplal} loses #{(100.0 * wplaw / (wplat == 0 ? 1 : wplat)).r2} % win #{wplap.r2.commify} profit</div></td>"
+			ta	<<	"<td>#{gaptitle}#{wplhdiv} PL Home #{wplhw} wins #{wplhl} loses #{(100.0 * wplhw / (wplht == 0 ? 1 : wplht)).r2} % win #{wplhp.r2.commify} profit#{Ediv}</td>"
+			ta	<<	"<td>#{gaptitle}#{wpladiv} PL Away #{wplaw} wins #{wplal} loses #{(100.0 * wplaw / (wplat == 0 ? 1 : wplat)).r2} % win #{wplap.r2.commify} profit#{Ediv}</td>"
 		else
-			ta	<<	"<td>#{gaptitle}#{watsdiv} #{watsw} ATS wins #{watsl} loses #{(100.0 * watsw / (watst == 0 ? 1 : watst)).r2} % win #{(watsw - 1.1 * watsl).r2.commify} profit</div></td>"
+			ta	<<	"<td>#{gaptitle}#{watsdiv} #{watsw} ATS wins #{watsl} loses #{(100.0 * watsw / (watst == 0 ? 1 : watst)).r2} % win #{(watsw - 1.1 * watsl).r2.commify} profit#{Ediv}</td>"
 		end
-		ta	<<	"<td>#{gaptitle}#{woudiv} #{wouright} OU wins #{woul} loses #{(100.0 * wouright / (wout == 0 ? 1 : wout)).r2} % win #{(wouright - 1.1 * woul).r2.commify} profit </div></td>"
-		ta	<<	"<td>#{gaptitle}#{wmldiv} Money Line #{wmlw} wins #{wmll.commify} loses #{(100.0 * wmlw / ((wmlw + wmll) == 0 ? 1 : wmlw + wmll)).r2} % win #{wmlp.r2.commify} profit </div></td>"
+		ta	<<	"<td>#{gaptitle}#{woudiv} #{wouright} OU wins #{woul} loses #{(100.0 * wouright / (wout == 0 ? 1 : wout)).r2} % win #{(wouright - 1.1 * woul).r2.commify} profit #{Ediv}</td>"
+		ta	<<	"<td>#{gaptitle}#{wmldiv} Money Line #{wmlw} wins #{wmll.commify} loses #{(100.0 * wmlw / ((wmlw + wmll) == 0 ? 1 : wmlw + wmll)).r2} % win #{wmlp.r2.commify} profit #{Ediv}</td>"
 
 		# year
-		ta	<<	"<td>Year#{ysudiv} #{week} #{ysuw} SU wins #{ysul} loses #{ypcw} % win </div></td>"
+		ta	<<	"<td>Year#{ysudiv} #{week} #{ysuw} SU wins #{ysul} loses #{ypcw} % win #{Ediv}</td>"
 		if (yplhw+wplhl+wplaw+yplal) > 0
-			ta	<<	"<td>Year #{yplhdiv}PL Home #{yplhw} wins #{yplhl} loses #{(100.0 * yplhw / (yplht == 0 ? 1 : yplht)).r2} % win #{yplhp.r2.commify} profit</div></td>"
-			ta	<<	"<td>Year #{ypladiv}PL Away #{yplaw} wins #{yplal} loses #{(100.0 * yplaw / (yplat == 0 ? 1 : yplat)).r2} % win #{yplap.r2.commify} profit</div></td>"
+			ta	<<	"<td>Year #{yplhdiv}PL Home #{yplhw} wins #{yplhl} loses #{(100.0 * yplhw / (yplht == 0 ? 1 : yplht)).r2} % win #{yplhp.r2.commify} profit#{Ediv}</td>"
+			ta	<<	"<td>Year #{ypladiv}PL Away #{yplaw} wins #{yplal} loses #{(100.0 * yplaw / (yplat == 0 ? 1 : yplat)).r2} % win #{yplap.r2.commify} profit#{Ediv}</td>"
 		else
-			ta	<<	"<td>#{yatsdiv}ATS #{yatsw} wins #{yatsl} loses #{(100.0 * yatsw / (yatst == 0 ? 1 : yatst)).r2} % win #{(yatsw - 1.1 * yatsl).r2.commify} profit</div></td>"
+			ta	<<	"<td>#{yatsdiv}ATS #{yatsw} wins #{yatsl} loses #{(100.0 * yatsw / (yatst == 0 ? 1 : yatst)).r2} % win #{(yatsw - 1.1 * yatsl).r2.commify} profit#{Ediv}</td>"
 		end
 		summ	+=	yatsw - 1.1 * yatsl
-		ta	<<	"<td>Year #{youdiv}OU #{youright} wins #{youl} loses % #{(100.0 * youright / (yout == 0 ? 1 : yout)).r2} win #{(youright - 1.1 * youl).r2.commify} profit</div></td>"
+		ta	<<	"<td>Year #{youdiv}OU #{youright} wins #{youl} loses % #{(100.0 * youright / (yout == 0 ? 1 : yout)).r2} win #{(youright - 1.1 * youl).r2.commify} profit#{Ediv}</td>"
 		summ	+=	youright - 1.1 * youl
-		ta	<<	"<td>Year #{ymldiv}Money Line #{ymlw} wins #{ymll.commify} loses #{(100.0 * ymlw / ((ymlw+ymll) == 0 ? 1 : (ymlw+ymll))).r2} % win #{ymlp.r2.commify} profit </div></td>"
+		ta	<<	"<td>Year #{ymldiv}Money Line #{ymlw} wins #{ymll.commify} loses #{(100.0 * ymlw / ((ymlw+ymll) == 0 ? 1 : (ymlw+ymll))).r2} % win #{ymlp.r2.commify} profit #{Ediv}</td>"
 		summ	+=	ymlp
 #		ta	<<	'</table></tr>' # turn off this table
 #		ta	<<	'<tr><table border="1">' # restart old table
@@ -126,29 +126,29 @@ def	ms(wsh, ysh, week, header=nil, gaptitle	=	'Week', gc	=	0)
 		outstr	=	"<tr><td>Home and Away Dog and Favorite Stats</td>"
 		p		=	(wsh['hd'][0]-wsh['hd'][1]*1.1).r2
 		sd		=	p	>	0.0	?	Gdiv : (p	==	0.0	?	Ydiv : Rdiv)
-		outstr	+=	"<td>#{sd}#{gaptitle}ly Home Dog Stats - #{wsh['hd'][0].commify} wins #{wsh['hd'][1].commify} loses - Profit is #{p.commify}</div></td>"
+		outstr	+=	"<td>#{sd}#{gaptitle}ly Home Dog Stats - #{wsh['hd'][0].commify} wins #{wsh['hd'][1].commify} loses - Profit is #{p.commify}#{Ediv}</td>"
 		p		=	(wsh['ad'][0]-wsh['ad'][1]*1.1).r2
 		sd		=	p	>	0.0	?	Gdiv : (p	==	0.0	?	Ydiv : Rdiv)
-		outstr	+=	"<td>#{sd}#{gaptitle}ly Road Dog Stats - #{wsh['ad'][0].commify} wins #{wsh['ad'][1].commify} loses - Profit is #{p.commify}</div></td>"
+		outstr	+=	"<td>#{sd}#{gaptitle}ly Road Dog Stats - #{wsh['ad'][0].commify} wins #{wsh['ad'][1].commify} loses - Profit is #{p.commify}#{Ediv}</td>"
 		p		=	(wsh['hf'][0]-wsh['hf'][1]*1.1).r2
 		sd		=	p	>	0.0	?	Gdiv : (p	==	0.0	?	Ydiv : Rdiv)
-		outstr	+=	"<td>#{sd}#{gaptitle}ly Home Fav Stats - #{wsh['hf'][0].commify} wins #{wsh['hf'][1].commify} loses - Profit is #{p.commify}</div></td>"
+		outstr	+=	"<td>#{sd}#{gaptitle}ly Home Fav Stats - #{wsh['hf'][0].commify} wins #{wsh['hf'][1].commify} loses - Profit is #{p.commify}#{Ediv}</td>"
 		p		=	(wsh['af'][0]-wsh['af'][1]*1.1).r2
 		sd		=	p	>	0.0	?	Gdiv : (p	==	0.0	?	Ydiv : Rdiv)
-		outstr	+=	"<td>#{sd}#{gaptitle}ly Road Fav Stats - #{wsh['af'][0].commify} wins #{wsh['af'][1].commify} loses - Profit is #{p.commify}</div></td>"
+		outstr	+=	"<td>#{sd}#{gaptitle}ly Road Fav Stats - #{wsh['af'][0].commify} wins #{wsh['af'][1].commify} loses - Profit is #{p.commify}#{Ediv}</td>"
 		outstr.gsub!('Dayly','Daily') if outstr.include?('Dayly')
 		p		=	(ysh['hd'][0]-ysh['hd'][1]*1.1).r2
 		sd		=	p	>	0.0	?	Gdiv : (p	==	0.0	?	Ydiv : Rdiv)
-		outstr	+=	"<td>#{sd}Yearly Home Dog Stats - #{ysh['hd'][0].commify} wins #{ysh['hd'][1].commify} loses - Profit is #{p.commify}</div></td>"
+		outstr	+=	"<td>#{sd}Yearly Home Dog Stats - #{ysh['hd'][0].commify} wins #{ysh['hd'][1].commify} loses - Profit is #{p.commify}#{Ediv}</td>"
 		p		=	(ysh['ad'][0]-ysh['ad'][1]*1.1).r2
 		sd		=	p	>	0.0	?	Gdiv : (p	==	0.0	?	Ydiv : Rdiv)
-		outstr	+=	"<td>#{sd}Yearly Road Dog Stats - #{ysh['ad'][0].commify} wins #{ysh['ad'][1].commify} loses - Profit is #{p.commify}</div></td>"
+		outstr	+=	"<td>#{sd}Yearly Road Dog Stats - #{ysh['ad'][0].commify} wins #{ysh['ad'][1].commify} loses - Profit is #{p.commify}#{Ediv}</td>"
 		p		=	(ysh['hf'][0]-ysh['hf'][1]*1.1).r2
 		sd		=	p	>	0.0	?	Gdiv : (p	==	0.0	?	Ydiv : Rdiv)
-		outstr	+=	"<td>#{sd}Yearly Home Fav Stats - #{ysh['hf'][0].commify} wins #{ysh['hf'][1].commify} loses - Profit is #{p.commify}</div></td>"
+		outstr	+=	"<td>#{sd}Yearly Home Fav Stats - #{ysh['hf'][0].commify} wins #{ysh['hf'][1].commify} loses - Profit is #{p.commify}#{Ediv}</td>"
 		p		=	(ysh['af'][0]-ysh['af'][1]*1.1).r2
 		sd		=	p	>	0.0	?	Gdiv : (p	==	0.0	?	Ydiv : Rdiv)
-		outstr	+=	"<td>#{sd}Yearly Road Fav Stats - #{ysh['af'][0].commify} wins #{ysh['af'][1].commify} loses - Profit is #{p.commify}</div></td>"
+		outstr	+=	"<td>#{sd}Yearly Road Fav Stats - #{ysh['af'][0].commify} wins #{ysh['af'][1].commify} loses - Profit is #{p.commify}#{Ediv}</td>"
 		outstr	+=	"</tr>"
 #		raise "summ #{summ} outstr #{outstr.inspect}"
 		ta	<<	outstr
