7f5ab16f5438bdf294f09d8077637772f189e5c5
Terry Smith <tersmi1@gmail.com>
2009-08-29 21:35:24 -0400

    just beautified it a bit

--- db/migrate/034_add_mlb.rb
+++ db/migrate/034_add_mlb.rb
@@ -3,7 +3,16 @@ class AddMlb < ActiveRecord::Migration
 	extend MigrationHelpers
 	def self.up
 		league = "Major League Baseball"
-		ut = ["Arizona Diamondbacks", "Atlanta Braves", "Baltimore Orioles", "Boston Red Sox", "Chicago Cubs", "Chicago White Sox", "Cincinnati Reds", "Cleveland Indians", "Colorado Rockies", "Detroit Tigers", "Florida Marlins", "Houston Astros", "Kansas City Royals", "Los Angeles Angels", "Los Angeles Dodgers", "Milwaukee Brewers", "Minnesota Twins", "New York Mets", "New York Yankees", "Oakland Athletics", "Philadelphia Phillies", "Pittsburgh Pirates", "San Diego Padres", "San Francisco Giants", "Seattle Mariners", "St. Louis Cardinals", "Tampa Bay Devil Rays", "Texas Rangers", "Toronto Blue Jays", "Washington Nationals"]
+		ut = ["Arizona Diamondbacks", "Atlanta Braves", "Baltimore Orioles", 
+			"Boston Red Sox", "Chicago Cubs", "Chicago White Sox", 
+			"Cincinnati Reds", "Cleveland Indians", "Colorado Rockies", 
+			"Detroit Tigers", "Florida Marlins", "Houston Astros", 
+			"Kansas City Royals", "Los Angeles Angels", "Los Angeles Dodgers", 
+			"Milwaukee Brewers", "Minnesota Twins", "New York Mets", 
+			"New York Yankees", "Oakland Athletics", "Philadelphia Phillies", 
+			"Pittsburgh Pirates", "San Diego Padres", "San Francisco Giants", 
+			"Seattle Mariners", "St. Louis Cardinals", "Tampa Bay Devil Rays", 
+			"Texas Rangers", "Toronto Blue Jays", "Washington Nationals"]
 		l	=	League.find_by_name(league)
 		League.create(:name=>league)	if l.nil?
 		ut.each{|tt|
